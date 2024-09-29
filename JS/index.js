1819361528b92498809d6f6f9dd97192dde05e5f
Ahmed Refai <momo3956248@gmail.com>
2024-09-30 02:45:27 +0300

    Cheked Email

--- JS/index.js
+++ JS/index.js
@@ -19,6 +19,9 @@ let emailRgx = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 let passwordRgx =
   /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/;
 
+let errorEmail = document.createElement('p');
+errorEmail.textContent = 'plase enter anothrt email';
+errorEmail.classList.add('text-danger');
 let allData = JSON.parse(localStorage.getItem('dataStorgeUser')) || [];
 
 //* Function
@@ -27,7 +30,8 @@ function addUser() {
   if (
     validtion(nameUpInput, nameRgx) &&
     validtion(emailUpInput, emailRgx) &&
-    validtion(passUpInput, passwordRgx)
+    validtion(passUpInput, passwordRgx) &&
+    chekedTOFindEmail()
   ) {
     let dataUser = {
       name: nameUpInput.value,
@@ -46,6 +50,7 @@ function clearInput() {
   nameUpInput.value = '';
   emailUpInput.value = '';
   passUpInput.value = '';
+  errorEmail.classList.add('d-none');
 }
 function validtion(ele, rgx) {
   if (rgx.test(ele.value)) {
@@ -59,27 +64,52 @@ function validtion(ele, rgx) {
   }
 }
 
-// *Event
+function chekedTOFindEmail() {
+  for (let i = 0; i < allData.length; i++) {
+    if (emailUpInput.value === allData[i].email) {
+      emailUpInput.value = '';
+      emailUpInput.after(errorEmail);
+      emailUpInput.classList.add('is-invalid');
+      return false;
+    } else {
+      return true;
+    }
+  }
+}
 
-// signUp.addEventListener('click', addUser);
-// emailUpInput.addEventListener('input', function () {
-//   validtion(emailUpInput, emailRgx);
-// });
-// nameUpInput.addEventListener('input', function () {
-//   validtion(nameUpInput, nameRgx);
-// });
-// passUpInput.addEventListener('input', function () {
-//   validtion(passUpInput, passwordRgx);
-// });
-
-// login.addEventListener('click', function () {
-//   console.log(emailInInput.value);
-// }); 
-
-// console.log(allData);
-// function checkedUser() {
+// chekedTOFindEmail();
+// function checkToFindUser() {
 //   for (let i = 0; i < allData.length; i++) {
-//     console.log(allData[i]);
+//     console.log(allData[i].email);
+//     // if (allData[i].email === emailInInput.vlaue) {
+//     //   console.log(true);
+//     // }
+//     // console.log(false);
 //   }
 // }
-// login.addEventListener('click', checkedUser);
+
+// *Event
+
+/* signUp.addEventListener('click', addUser);
+emailUpInput.addEventListener('input', function () {
+  validtion(emailUpInput, emailRgx);
+});
+nameUpInput.addEventListener('input', function () {
+  validtion(nameUpInput, nameRgx);
+});
+passUpInput.addEventListener('input', function () {
+  validtion(passUpInput, passwordRgx);
+});
+
+login.addEventListener('click', function () {
+  console.log(emailInInput.value);
+}); 
+
+console.log(allData);
+function checkedUser() {
+  for (let i = 0; i < allData.length; i++) {
+    console.log(allData[i]);
+  }
+}
+login.addEventListener('click', checkedUser);
+ */
